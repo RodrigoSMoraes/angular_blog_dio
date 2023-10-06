e8cf7a3dccc0cc0695400f20e80f461ac4ed340b
Rodrigo Moraes <rmoraes.engenharia@hotmail.com>
2023-10-06 20:19:53 -0300

    Feat: complete blog

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -1,14 +1,27 @@
-export const data = [
+export const dataFake = [
     {
-        "id": 1,
-        "photo": "aaa0",
-        "title": "titulo",
-        "description": "descrição"
+        "id": "1",
+        "photo": "https://variety.com/wp-content/uploads/2013/03/ironman31.jpg?w=1000&h=563&crop=1&resize=1000%2C563",
+        "title": "Robert Downey Jr: No Plans for ‘Iron Man 4’",
+        "description": "Dont hold your breath, Tony Stark fans.\n\nIn a new interview with Variety, Robert Downey Jr. shot down speculation about a fourth Iron Man movie.\n\nThere isnt one in the pipe, Downey said. No, theres no plan for a fourth Iron Man."
     },
     {
-        "id": 2,
-        "photo": "aaa0",
-        "title": "titulo",
-        "description": "descrição"
+        "id": "2",
+        "photo": "https://s2-g1.glbimg.com/uQx7uo5VpE58jI2_i3rI6Ax1OYs=/0x0:1700x1065/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/R/R/ZmHiLxSpSAgdWvWA5Vsw/thor-arte.jpg",
+        "title": "Cosplay de Thor, brasileiro aproveita semelhança com ator de Hollywood",
+        "description": "Thor é brasileiro, joga futebol, treina muay thai e faz dieta à base de proteínas para se manter em forma. Quando não está na função de herói, acorda cedo para trabalhar como fisioterapeuta em Orlândia (SP). \n ara a criança desavisada ou fã entusiasmado, o jovem Jorge Gabriel Grassi, de 28 anos, poderia ser facilmente confundido com o herdeiro de Asgard, ainda mais se estiver caracterizado ao lado do amigo Jonatan Silva Medeiros, na pele do personagem Loki, o imprevisível irmão do guerreiro."
     },
+    {
+        "id": "3",
+        "photo": "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/09/as_marvels_filme_mais_curto__j25gp31f0-1210x544.jpg",
+        "title": "As Marvels pode ser o filme mais curto do MCU",
+        "description": "O Marvel Studios se prepara para o lançamento de As Marvels, o próximo filme do estúdio a chegar aos cinemas. Curiosamente, a produção pode ser o lançamento mais curto do MCU.\n\nDe acordo com o Collider, a rede de cinemas norte-americana AMC divulgou que As Marvels terá 105 minutos de duração – ou seja, uma hora e 45 minutos. Isso torna o longa mais curto do MCU, superando O Incrível Hulk (2008) e Thor: Mundo Sombrio (2013), empatados com 112 minutos – ou, uma hora e 52 minutos."
+    },
+    {
+        "id": "4",
+        "photo": "https://epipoca.com.br/wp-content/uploads/2021/01/visao-mcu.jpg",
+        "title": "Os melhores momentos do Visão no MCU",
+        "description": "Paul Bettany começou no Universo Cinematográfico da Marvel emprestando sua voz para a inteligência artificial JARVIS. Mais tarde ele assumiu o posto do sintezoide Visão.\n\n Apesar de passar muitos anos interpretando um personagem secundário no MCU, agora Paul Bettany está pronto para ser um dos protagonistas de WandaVision, que chega no dia  de janeiro no Disney+. \n\nNa série Bettany voltará a dar vida ao Visão, e abaixo você pode conferir os melhores momentos do herói no cinema."
+    }
+    
 ]
--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -10,14 +11,31 @@ export class ContentComponent {
   photoCover:string=""
   contentTitle:string=""
   contentDescription:string=""
+  private id:string | null = ""
 
   constructor (
     private route:ActivatedRoute
   ) {}
+
   ngOnInit(): void {
-    this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+    this.route.paramMap.subscribe(value => {
+      const id = value.get("id");
+      if (id !== null) {
+        this.id = id;
+        this.setValuesComponent(this.id);
+      }
+    });
+  }
+
+  setValuesComponent(id:string ){ 
+    const result = dataFake.filter( 
+      ( article ) => article.id === id 
     )
+    console.log(result)
+    this.contentTitle = result[0].title;
+    this.photoCover = result[0].photo;
+    this.contentDescription = result[0].description;
+
   }
 
 }
